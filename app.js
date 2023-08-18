976345fd48cfa43e8130d8583c34a180a2a102dd
Devin Udy <34194818+Devin-The-Dev@users.noreply.github.com>
2023-08-18 16:53:55 -0600

    Removed index.js and the script tag in app.js

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.get('/', (req, res) => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script defer src="player.js"></script>
         <title>Monopoly Stats</title>
     </head>
     <body>
